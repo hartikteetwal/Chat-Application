c49272a9d25084ec498eefcf9ec30d1cbd44cf1a
hartik teetwal <hartik.teetwal@ezbizsoft.in>
2025-04-03 18:58:51 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SettingPage from './pages/SettingPage';
+import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className="text-red-500 text-3xl font-semibold">
-      apple
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/setting' element={<SettingPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
     </div>
   );
 };
