234c2e750c3a3d7d656fd4a63c8551fb0aa09474
hartik teetwal <hartik.teetwal@ezbizsoft.in>
2025-04-03 15:46:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <p className="text-red-500 text-3xl md:text-4xl font-semibold">
-        Hello, Tailwind with React!
-      </p>
+    <div className="text-red-500 text-3xl font-semibold">
+      apple
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
