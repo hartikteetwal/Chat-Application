fed7f79ac14936ec08c4ef2067516fbd391a7ebe
hartikteetwal <teetwalhartik@gmail.com>
2025-04-03 23:31:06 +0530

    changees

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 const LoginPage = () => {
+  const [showPassword,setShowPassword] = useState(false);
+  const [formData,setFormData] = useState({
+    fullName:"",
+    email:"",
+    password:""
+  })
+
+  
   return (
     <div>
           LoginPage
